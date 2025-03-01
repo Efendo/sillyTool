e8f781f48a197e60f06171eadaf9c5f506e311d1
Efendo <efe_mustafa.oener@icloud.com>
2025-03-01 18:42:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# sillyTool
+sillyTool is a silly lil tool to spam and be annoying
+
+I do not condone spamming innapropriate stuff using this.
+and I am not responsible if you get banned or something, use at your own risk!
+
+## How the .st file format works
+The .st file format (sillyTool sillyScript) is just a .txt but renamed so it looks fancy
+To seperate sentences/words, use newlines, heres an example
+
+```This is the first message
+This is the second message
+This is the third message
+---------------------------
+and
+this
+is
+seperated
+text```
---
+++ main.py
@@ -0,0 +1,89 @@
+import pyautogui as pag
+import time as t
+import os
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+tk.Tk().withdraw()
+
+options = [
+    "Generic Spammer",
+    "Spam from a sillyScript",
+    "Quit"
+]
+
+def parse(file):
+    text=[]
+    f=open(file, "r")
+    for x in f:
+            x = x.rstrip("\n")
+            text.append(x)
+    return text
+
+def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("""
+        _ _ _     _____         _ 
+    __(_) | |_  |_   _|__  ___| |
+    (_-< | | | || || |/ _ \/ _ \ |
+    /__/_|_|_|\_, ||_|\___/\___/_|
+            |__/ 
+    """, end="")
+    print("         By Efendo")
+    for i in range(0,len(options)):
+        print(f"{i}) {options[i]}")
+    print("")
+    selected = input("What do you want to do: ")
+    try:
+        if int(selected) == 0:
+            try:
+                GenericSpammer()
+            except:
+                print("Something went wrong! Going back to main menu!")
+                t.sleep(1)
+                main()
+        elif int(selected) == 1:
+            try:
+                SpamFromFile()
+            except:
+                print("Something went wrong! Going back to main menu!")
+                t.sleep(1)
+                main()
+    except ValueError:
+        print("Selection must be an Integer!")
+        t.sleep(1)
+        main()
+
+def GenericSpammer():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    message=input("Message: ")
+    beforesend=input("Key to send before typing message (leave empty for nothing): ")
+    interval=float(input("Interval: "))
+    beforeStart=int(input("Time before start: "))
+    repeat=int(input("How many messages: "))
+    t.sleep(beforeStart)
+    for i in range(0, repeat):
+        pag.press(beforesend)
+        pag.write(message)
+        pag.press("enter")
+        print(f"Sent message! {repeat-i} messages left.")
+        t.sleep(interval)
+    print("Finished!")
+    main()
+
+def SpamFromFile():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    file=askopenfilename()
+    messages=parse(file)
+    beforesend=input("Key to send before typing message (leave empty for nothing): ")
+    interval=float(input("Interval: "))
+    beforeStart=int(input("Time before start: "))
+    t.sleep(beforeStart)
+    for message in messages:
+        pag.press(beforesend)
+        pag.write(message)
+        pag.press("enter")
+        t.sleep(interval)
+    print("Finished!")
+    main()
+    
+main()
